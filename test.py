bed9156b2c0e78001b453002c404d645716137cc
mariamnassar19 <145570066+mariamnassar19@users.noreply.github.com>
2024-05-22 14:56:48 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
+# test_transformers.py
+from transformers import FlaubertTokenizer, FlaubertForSequenceClassification
+
+model_name = "mn00/trial"
+
+try:
+    model = FlaubertForSequenceClassification.from_pretrained(model_name)
+    tokenizer = FlaubertTokenizer.from_pretrained(model_name)
+    print("Transformers library and model loaded successfully.")
+except Exception as e:
+    print(f"Error loading transformers library: {e}")
 

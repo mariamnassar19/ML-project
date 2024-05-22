574697db73c425764c6f8fec1302c6883f8e272b
mariamnassar19 <145570066+mariamnassar19@users.noreply.github.com>
2024-05-22 19:40:17 +0200

    Update App3.py

--- App3.py
+++ App3.py
@@ -19,9 +19,8 @@ from pytube import YouTube
 nltk.download('stopwords')
 
 # Retrieve API key from Streamlit secrets
-youtube_api_key = st.secrets["YOUTUBE_API_KEY"]
+youtube_api_key = st.secrets["api"]["youtube_key"]
 youtube = build('youtube', 'v3', developerKey=youtube_api_key)
-
 
 def search_youtube_videos(query, max_results=10):
     """Search videos on YouTube based on the query."""
@@ -51,6 +50,7 @@ warnings.filterwarnings("ignore",
 
 # Load the model and tokenizer
 model_path = 'shiqi-017/flaubert'
+
 try:
     model = FlaubertForSequenceClassification.from_pretrained(model_path)
     tokenizer = FlaubertTokenizer.from_pretrained(model_path)
@@ -64,13 +64,13 @@ try:
         st.session_state.feedback_data = pd.DataFrame(columns=['sentence', 'difficulty'])
 
     # Define the Streamlit layout
-    st.title('Text Difficulty Prediction App')
+    st.title('Mon Ami Français')
     st.write(
-        'This application predicts the difficulty level of French sentences. You can upload a CSV file, input sentences directly, provide a YouTube video URL, or input long texts such as song lyrics.')
+        'This application predicts the difficulty level of French sentences. You can upload a CSV file, input sentences directly, or input long texts such as song lyrics.')
 
-    # Tab layout for file upload, text input, long text input, YouTube video input, YouTube videos by difficulty, and feedback
-    tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(
-        ["Upload CSV", "Input Sentence", "Input Long Text", "YouTube Video URL", "YouTube Videos by Difficulty", "Feedback"])
+    # Tab layout for file upload, text input, long text input, YouTube videos by difficulty, and feedback
+    tab1, tab2, tab3, tab4, tab5 = st.tabs(
+        ["Upload CSV", "Input Sentence", "Input Long Text", "YouTube Videos by Difficulty", "Feedback"])
 
     with tab1:
         st.header("Upload CSV File")
@@ -178,50 +178,6 @@ try:
                     st.pyplot()
 
     with tab4:
-        st.header("YouTube Video URL")
-        youtube_url = st.text_input("Enter YouTube URL here:")
-        if st.button("Predict Difficulty from Video"):
-            if youtube_url:
-                with st.spinner('Processing YouTube video...'):
-                    try:
-                        # Download the YouTube video
-                        yt = YouTube(youtube_url)
-                        audio_stream = yt.streams.filter(only_audio=True).first()
-                        audio_file = audio_stream.download(filename="audio.mp4")
-
-                        # Convert audio to WAV format using ffmpeg
-                        wav_file = "audio.wav"
-                        ffmpeg.input(audio_file).output(wav_file).run(overwrite_output=True)
-                        os.remove(audio_file)  # Remove original audio file
-
-                        # Transcribe audio to text
-                        recognizer = sr.Recognizer()
-                        with sr.AudioFile(wav_file) as source:
-                            audio_data = recognizer.record(source)
-                            transcribed_text = recognizer.recognize_google(audio_data, language="fr-FR")
-                        os.remove(wav_file)  # Remove WAV file after transcription
-
-                        st.write("Transcription:")
-                        st.write(transcribed_text)
-
-                        # Process the transcribed text
-                        dataset = Dataset.from_pandas(pd.DataFrame({'sentence': [transcribed_text]}))
-                        dataset = dataset.map(
-                            lambda examples: tokenizer(examples['sentence'], padding="max_length", truncation=True,
-                                                       max_length=512), batched=True)
-                        dataset.set_format('torch', columns=['input_ids', 'attention_mask'])
-                        predictions = trainer.predict(dataset).predictions
-                        predicted_class = predictions.argmax(axis=1)
-                        predicted_difficulty = difficulty_mapping[predicted_class[0]]
-
-                        st.success(f'The predicted difficulty level for the transcribed video is: {predicted_difficulty}')
-                    except Exception as video_error:
-                        st.error(f"Error processing video: {video_error}")
-                        traceback.print_exc()
-            else:
-                st.error("Please enter a YouTube URL for prediction.")
-
-    with tab5:
         st.header("Find YouTube Videos by Difficulty")
         difficulty_level = st.selectbox("Select the difficulty level:", ('A1', 'A2', 'B1', 'B2', 'C1', 'C2'), index=2)
         if st.button('Search Videos'):
@@ -229,7 +185,7 @@ try:
             videos = search_youtube_videos(query)
             display_video_results(videos)
 
-    with tab6:
+    with tab5:
         st.header("Feedback")
         feedback_sentence = st.text_area("Please provide a sentence for feedback:")
         feedback_difficulty = st.selectbox("Select the difficulty level:", ('A1', 'A2', 'B1', 'B2', 'C1', 'C2'))

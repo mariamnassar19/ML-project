dcbeb5643bb528e4c14b2df5d716fc982628616b
mariamnassar19 <145570066+mariamnassar19@users.noreply.github.com>
2024-05-22 19:04:02 +0200

    Update youtube_video_url.py

--- pages/youtube_video_url.py
+++ pages/youtube_video_url.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from pytube import YouTube
-import whisper
+import speech_recognition as sr
+import ffmpeg
 import os
 import pandas as pd
 from datasets import Dataset
@@ -18,11 +19,17 @@ def render(model, tokenizer, trainer):
                     audio_stream = yt.streams.filter(only_audio=True).first()
                     audio_file = audio_stream.download(filename="audio.mp4")
 
+                    # Convert audio to WAV format using ffmpeg
+                    wav_file = "audio.wav"
+                    ffmpeg.input(audio_file).output(wav_file).run(overwrite_output=True)
+                    os.remove(audio_file)  # Remove original audio file
+
                     # Transcribe audio to text
-                    whisper_model = whisper.load_model("base")
-                    transcription = whisper_model.transcribe(audio_file)
-                    transcribed_text = transcription['text']
-                    os.remove(audio_file)  # Remove audio file after transcription
+                    recognizer = sr.Recognizer()
+                    with sr.AudioFile(wav_file) as source:
+                        audio_data = recognizer.record(source)
+                        transcribed_text = recognizer.recognize_google(audio_data, language="fr-FR")
+                    os.remove(wav_file)  # Remove WAV file after transcription
 
                     st.write("Transcription:")
                     st.write(transcribed_text)
@@ -37,8 +44,7 @@ def render(model, tokenizer, trainer):
                     predicted_class = predictions.argmax(axis=1)
                     predicted_difficulty = difficulty_mapping[predicted_class[0]]
 
-                    st.success(
-                        f'The predicted difficulty level for the transcribed video is: {predicted_difficulty}')
+                    st.success(f'The predicted difficulty level for the transcribed video is: {predicted_difficulty}')
                 except Exception as video_error:
                     st.error(f"Error processing video: {video_error}")
                     traceback.print_exc()
